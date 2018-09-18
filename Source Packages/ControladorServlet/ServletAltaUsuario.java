c6a6605ae4a6e45557fc7c553e8ae7ba68e39a61
Lucas <Lucas@DESKTOP-CUER15B>
2018-09-18 19:56:40 -0300

    Merge origin/master
    Conflicts:
    	.gitignore
    	build.xml
    	nbproject/build-impl.xml
    	nbproject/genfiles.properties
    	nbproject/project.properties
    	nbproject/project.xml

--- Source Packages/ControladorServlet/ServletAltaUsuario.java
+++ Source Packages/ControladorServlet/ServletAltaUsuario.java
@@ -17,7 +17,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(name = "ServletAltaUsuario", urlPatterns = {"/altaUsuarioServlet"})
+@WebServlet("/registrarse")
 public class ServletAltaUsuario extends HttpServlet {
 
     /**
@@ -29,11 +29,24 @@ public class ServletAltaUsuario extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-    public String MENSAJE = null;
-    
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        
-        request.getRequestDispatcher("Vistas/altaUsuario.jsp").forward(request, response);
+    public static final String MENSAJE_ERROR = "mensaje_error";
+    public static final String MENSAJE_EXITO = "mensaje_exito";
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet ServletAltaUsuario</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet ServletAltaUsuario at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
@@ -61,62 +74,70 @@ public class ServletAltaUsuario extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String operacion = request.getParameter("op");
-        switch (operacion) {
-            case "1":
-                String nick = request.getParameter("nick");
-                String nombre = request.getParameter("nombre");
-                String apellido = request.getParameter("apellido");
-                String correo = request.getParameter("correo");
-                String pass = request.getParameter("pass");
-                String fecha = (request.getParameter("fecha") == null ? "" : request.getParameter("fecha"));
-                String pass2 = request.getParameter("pass2");
-                String direccion = request.getParameter("direccion");
-                String sitio = request.getParameter("sitio");
-                String biografia = request.getParameter("biografia");
-                String tipoP = request.getParameter("tipo");
-                String imagen = "";
-                
-                if (tipoP == "proponente") {
-                    Calendar cal;
-                    DateFormat format = new SimpleDateFormat("yyyy/mm/dd");
-                    format.format(fecha);
-                    cal = format.getCalendar();
-                    try {
-                        logica.Controladores.ControladorUsuario.getInstance().AgregarUsuarioProponente(nick, nombre, apellido, correo, cal, imagen, direccion, biografia, sitio, pass);
-                        MENSAJE = "Se registro exitosamente";
-                        request.setAttribute("mensaje", MENSAJE);
-                        request.getRequestDispatcher("/Vistas/altaUsuario.jsp").include(request, response);
-                    } // try
-                    catch (ExceptionInInitializerError | Exception a) {
-                        MENSAJE = "Error al registrar este usuario";
-                        request.getSession().setAttribute("mensaje", MENSAJE);
-                        request.getRequestDispatcher("/Vistas/altaUsuario.jsp").forward(request, response);
-                    } // catch           
+        String nick = request.getParameter("nick");
+        String nombre = request.getParameter("nombre");
+        String apellido = request.getParameter("apellido");
+        String correo = request.getParameter("correo");
+        String pass = request.getParameter("pass");
+        String fecha = (request.getParameter("fecha") == null ? "" : request.getParameter("fecha"));
+        String confPass = request.getParameter("confPass");
+        String direccion = request.getParameter("direccion");
+        String sitio = request.getParameter("sitio");
+        String biografia = request.getParameter("biografia");
+        String tipoP = request.getParameter("tipo");
+        String imagen = "";
 
-                } else {
-                    Calendar cal;
-                    DateFormat format = new SimpleDateFormat("yyyy/mm/dd");
-                    format.format(fecha);
-                    cal = format.getCalendar();
-                    try {
-                        logica.Controladores.ControladorUsuario.getInstance().AgregarUsuarioColaborador(nick, nombre, apellido, correo, cal, pass, pass);
-                        MENSAJE = "Se registro exitosamente";
-                        request.getSession().setAttribute("mensaje", MENSAJE);
-                        request.getSession().setAttribute("nick", nick);
-                        request.getRequestDispatcher("/Vistas/altaUsuario.jsp").include(request, response);
-                    } // try
-                    catch (ExceptionInInitializerError | Exception a) {
-                        MENSAJE = "Error al dar registrar este usuario";
-                        request.getSession().setAttribute("mensaje", MENSAJE);
-                        request.getRequestDispatcher("/Vistas/altaUsuario.jsp").forward(request, response);
-                    } // catch           
-                }
-                
-                processRequest(request, response);
-                break;
+        if (tipoP == "proponente") {
+            if (!nick.equals("") && !apellido.equals("") && !correo.equals("") && !fecha.equals("") && !pass.equals("") && !confPass.equals("")) {
+                Calendar cal;
+                DateFormat format = new SimpleDateFormat("yyyy/mm/dd");
+                format.format(fecha);
+                cal = format.getCalendar();
+                try {
+                    logica.Controladores.ControladorUsuario.getInstance().AgregarUsuarioProponente(nick, nombre, apellido, correo, cal, imagen, direccion, biografia, sitio, pass);
+                    String mensajeExito = "Imagen subida correctamente";
+                    request.getSession().setAttribute(MENSAJE_EXITO, mensajeExito);
+                    request.getSession().setAttribute("nick", nick);
+                    request.getRequestDispatcher("/Vistas/exito.jsp").include(request, response);
+                } // try
+                catch (ExceptionInInitializerError | Exception a) {
+                    String mensajeError = "Error al dar registrar este usuario";
+                    request.getSession().setAttribute(MENSAJE_ERROR, mensajeError);
+                    request.getRequestDispatcher("/Vistas/error.jsp").forward(request, response);
+                } // catch           
+
+            } else {
+                String mensajeError = "Debe completar todos los campos obligatorios";
+                request.getSession().setAttribute(MENSAJE_ERROR, mensajeError);
+                request.getRequestDispatcher("").forward(request, response);
+            }
+        } else {
+            if (!nick.equals("") && !apellido.equals("") && !correo.equals("") && !fecha.equals("") && !pass.equals("") && !confPass.equals("") && !sitio.equals("") && !direccion.equals("") && !biografia.equals("")) {
+
+                Calendar cal;
+                DateFormat format = new SimpleDateFormat("yyyy/mm/dd");
+                format.format(fecha);
+                cal = format.getCalendar();
+                try {
+                    logica.Controladores.ControladorUsuario.getInstance().AgregarUsuarioColaborador(nick, nombre, apellido, correo, cal, pass, pass);
+                    String mensajeExito = "Imagen subida correctamente";
+                    request.getSession().setAttribute(MENSAJE_EXITO, mensajeExito);
+                    request.getSession().setAttribute("nick", nick);
+                    request.getRequestDispatcher("/Vistas/exito.jsp").include(request, response);
+                } // try
+                catch (ExceptionInInitializerError | Exception a) {
+                    String mensajeError = "Error al dar registrar este usuario";
+                    request.getSession().setAttribute(MENSAJE_ERROR, mensajeError);
+                    request.getRequestDispatcher("/Vistas/error.jsp").forward(request, response);
+                } // catch           
+
+            } else {
+                String mensajeError = "Debe completar todos los campos obligatorios";
+                request.getSession().setAttribute(MENSAJE_ERROR, mensajeError);
+                request.getRequestDispatcher("/Vistas/error.jsp").forward(request, response);
+            }
         }
-        
+        processRequest(request, response);
     }
 
     /**
--- build/web/Vistas/altaUsuario.jsp
+++ build/web/Vistas/altaUsuario.jsp
@@ -1,4 +1,5 @@
 <%@page import="com.sun.java.swing.plaf.windows.resources.windows"%>
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
     <head>
@@ -7,42 +8,32 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" type="text/css" href="../css/AltaUsuarioCSS.css">
         <script type="text/javascript" src="../js/validacion.js"></script>
+        <script type="text/javascript" src="../js/funcionesAltaUsuario.js"></script>
 
         <%
-            String mensaje = (String) request.getSession().getAttribute("mensaje");
+            String contraseniasDiferentes = (String) request.getAttribute("mensaje");
+            if (contraseniasDiferentes != null) {%>
+        <script type="text/javascript">
+            window.alert("<%=contraseniasDiferentes%>");
+        </script>
+        <% }%>
+
+        <%
+            String mensaje = (String) request.getAttribute("mensaje");
             if (mensaje != null) {%>
         <script type="text/javascript">
-            alert("<%= mensaje%>");
+            window.alert("<%= mensaje%>");
+            location.href = "/CulturarteWeb/ServletInicio";
         </script>
         <%  }
         %>
         <script type="text/javascript">
-            function enviar() {
-            $.ajax({
-            type: 'POST', //va por post 
-                    url: "/altaUsuarioServlet",
-                    data: {
-                    var op=1;
-                    var nick = $("#nick").val().toString();
-                    var nombre = $("#nombre").val().toString();
-                    var apellido = $("#apellido").val().toString();
-                    var correo = $("#correo").val().toString();
-                    var pass = $("#pass").val().toString();
-                    var pass2 = $("#pass2").val().toString();
-                    var tipo = $("#tipo").val().toString();
-                    var direccion = $("#direccion").val().toString();
-                    var sitio = $("#sitio").val().toString();
-                    var biografia = $("#biografia").val().toString(); },
-                    success: function (data) {
-                    window.alert("Usted se a registrado con exito");
-                    }
-            error: function (){
-            alert("Error al intentar registrarse");
-            }}); }
-
-            function mostrarDatos(){
-            var elemento = document.getElementById("adicionales");
-            elemento.style.display = 'block';
+            function mostrarDatos() {
+                var elemento = document.getElementById("otros");
+                if (elemento.style.display === 'block')
+                    document.getElementById("otros").style.display = "none";
+                else
+                    document.getElementById("otros").style.display = "block";
             }
         </script>
 
@@ -50,58 +41,57 @@
     <body>
         <div style="padding-left: 444px" class="form">
             <h1>Registrarte</h1>
-            <h4>Es gratis y lo sera siempre.</h4>
-            <form onsubmit="enviar()">
+            <form action="altaUsuarioServlet" method="post" onsubmit="validarDatos()">
                 <div>
                     <label for="name">Nombre de usuario:</label>
-                    <input id="nick" class="form-control"placeholder="Escribe tu nombre de usuario" type="text" required/>
+                    <input name="nick" class="form-control"placeholder="Escribe tu nombre de usuario" type="text" required/>
                 </div>
                 <br>
                 <div>
                     <label for="name">Nombre:</label>
-                    <input id="nombre" class="form-control"placeholder="Escribe tu nombre de usuario" type="text" required/>
+                    <input name="nombre" class="form-control"placeholder="Escribe tu nombre de usuario" type="text" required/>
                 </div>
                 <br>
                 <div>
                     <label for="name">Apellido:</label>
-                    <input id="apellido" class="form-control"placeholder="Escribe apellido" type="text" required/>
+                    <input name="apellido" class="form-control"placeholder="Escribe apellido" type="text" required/>
                 </div>
                 <br>
                 <div>
                     <label for="mail">E-mail:</label>
-                    <input id="correo" class="form-control" placeholder="Escribe tu correo electrónico" type="email" required/>
+                    <input name="correo" class="form-control" placeholder="Escribe tu correo electrÃ³nico" type="email" required/>
                 </div>
                 <br>
                 <div>
-                    <label for="contrasenia">Contraseña:</label>
-                    <input id="pass" class="form-control" placeholder="Escribe una contraseña" type="password" required/>
+                    <label for="contrasenia">ContraseÃ±a:</label>
+                    <input name="pass" class="form-control" placeholder="Escribe una contraseÃ±a" type="password" required/>
                     &nbsp;&nbsp;&nbsp;&nbsp;
-                    <label for="contrasenia">Confirmar contraseña:</label>
-                    <input id="pass2" class="form-control" placeholder="Vuelve a escribir tu contraseña" type="password" required/>
+                    <label for="contrasenia">Confirmar contraseÃ±a:</label>
+                    <input name="pass2" class="form-control" placeholder="Vuelve a escribir tu contraseÃ±a" type="password" required/>
                 </div>
                 <br>
                 <div>
                     <label for="fechaN">Fecha de nacimiento</label>
-                    <input id="fecha" class="form-control" type="date" required>
+                    <input name="fecha" class="form-control" type="date" required>
                 </div>
                 <br>
                 <div>
                     <label>Tipo de persona</label><br>
-                    <input  id="tipo" name="tipoP"  type="radio"  value="colaborador">Soy un colaborador &nbsp;&nbsp;&nbsp;
-                    <input onclick="mostrarDatos()" name="tipoP" id="tipo"  type="radio"  value="proponente">Soy un proponente<br>
+                    <input onclick="mostrarDatos()" name="tipo" name="tipoP"  type="radio"  value="colaborador" checked>Soy un colaborador &nbsp;&nbsp;&nbsp;
+                    <input onclick="mostrarDatos()" name="tipo" name="tipoP"  type="radio"  value="proponente">Soy un proponente<br>
                 </div>
                 <br>
-                <div id="adicionales" style="display: none">
+                <div name="otros" style="display: none">
                     <div>
                         <label for="direccion">Direccion</label>
-                        <input class="form-control" id="direccion" type="text" />
+                        <input class="form-control" name="direccion" type="text" />
                         &nbsp;&nbsp;&nbsp;&nbsp;
                         <label for="sitio">Sitio web</label>
-                        <input class="form-control"  id="sitio" type="text" />
+                        <input class="form-control"  name="sitio" type="text" />
                     </div><br><br><br>
                     <div>
                         <p>Biografia:
-                            <textarea class="form-control" id="biografia"></textarea>
+                            <textarea class="form-control" name="biografia"></textarea>
                         </p>
                     </div>
                 </div>
